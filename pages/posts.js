bf4257549ec1ce103c7b1eabb8c53908497e72d5
amelmnd <amelmnd.dev@gmail.com>
2024-07-06 19:20:46 +0200

    add connection api for posts

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-export default function posts() {
+export default function posts(props) {
+  console.log(props);
   return (
     <div>
       <h1>ALL POST</h1>
+      {props.posts?.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+        </div>
+      ))}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await data.json();
+  return {
+    props: {
+      posts,
+    },
+  };
 }
