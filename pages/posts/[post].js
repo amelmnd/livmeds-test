3150e2abc2c62ada324c662854a17ad3dd10919b
amelmnd <amelmnd.dev@gmail.com>
2024-07-08 09:42:29 +0200

    security add try catch

--- pages/posts/[post].js
+++ pages/posts/[post].js
@@ -10,6 +10,8 @@ export default function Post(props) {
     <div>
       {status != 'authenticated' ? (
         error403()
+      ) : !props.postData || !props.commentsData ? (
+        error404()
       ) : (
         <div>
           <h1> {props.postData.title}</h1>
@@ -30,32 +32,72 @@ export default function Post(props) {
 }
 
 export async function getStaticProps(context) {
-  const id = context.params.post;
-  const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  const postData = await post.json();
+  try {
+    const id = context.params.post;
+    if (!id) throw new Error(`id was not ok: ${post.statusText}`);
 
-  const comments = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}/comments`
-  );
-  const commentsData = await comments.json();
+    const post = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}`
+    );
 
-  return {
-    props: {
-      postData,
-      commentsData,
-    },
-  };
+    if (!post.ok) throw new Error(`Post was not ok: ${post.statusText}`);
+
+    const postData = await post.json();
+
+    if (!postData) throw new Error('Invalid post data format');
+
+    const comments = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+    );
+
+    if (!comments.ok)
+      throw new Error(`Comment was not ok : ${comments.statusText}`);
+
+    const commentsData = await comments.json();
+
+    if (!commentsData) throw new Error('Invalid comments data format');
+
+    return {
+      props: {
+        postData,
+        commentsData,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        postData: null,
+        commentsData: [],
+        error: 'Failed to fetch post or comments',
+      },
+    };
+  }
 }
 
 export async function getStaticPaths() {
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const posts = await data.json();
-  const paths = posts.map((post) => ({
-    params: { post: post.id.toString() },
-  }));
+  try {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts');
 
-  return {
-    paths,
-    fallback: false,
-  };
+    if (!data.ok) throw new Error(`Data was not ok: ${response.statusText}`);
+
+    const posts = await data.json();
+
+    if (!Array.isArray(posts)) {
+      throw new Error('Invalid data format');
+    }
+
+    const paths = posts.map((post) => ({
+      params: { post: post.id.toString() },
+    }));
+
+    return {
+      paths,
+      fallback: false,
+    };
+  } catch (error) {
+    return {
+      paths: [],
+      fallback: false,
+    };
+  }
 }
